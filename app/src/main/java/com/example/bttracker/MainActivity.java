c39bea410edaa0787d110d9e46a09371c3194900
ippei94da <ippei94da@gmail.com>
2020-05-29 10:33:14 +0900

    initial
    test

---
+++ app/src/main/java/com/example/bttracker/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.bttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gotoLogActivity(View view) {
+        Intent toLog = new Intent(this, LogActivity.class);
+        startActivity(toLog);
+
+    }
+
+    public void goToNormalActivity(View view) {
+        Intent toNormal = new Intent(this, NormalActivity.class);
+        startActivity(toNormal);
+    }
+
+    public void goToMechanismActivity(View view) {
+        Intent toMechanism = new Intent(this, MechanismActivity.class);
+        startActivity(toMechanism);
+    }
+
+    public void GoToLink2(View view) {
+        Intent toLink2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
+        startActivity (toLink2);
+    }
+}
