8070b9cce364f78bcd921054949984e5ab02ad2f
ippei94da <ippei94da@gmail.com>
2020-06-04 16:30:04 +0900

    implemented OnClickListener

--- app/src/main/java/com/example/bttracker/MainActivity.java
+++ app/src/main/java/com/example/bttracker/MainActivity.java
@@ -6,34 +6,54 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mainButton1 = findViewById(R.id.MainButton1);
+        mainButton1.setOnClickListener(this);
+
+        TextView mainButton2 = findViewById(R.id.MainButton2);
+        mainButton2.setOnClickListener(this);
+
+        TextView mainButton3 = findViewById(R.id.MainButton3);
+        mainButton3.setOnClickListener(this);
+
+        TextView mainButton4 = findViewById(R.id.MainButton4);
+        mainButton4.setOnClickListener(this);
     }
 
-    public void gotoLogActivity(View view) {
-        Intent toLog = new Intent(this, LogActivity.class);
-        startActivity(toLog);
-
-    }
-
-    public void goToNormalActivity(View view) {
-        Intent toNormal = new Intent(this, NormalActivity.class);
-        startActivity(toNormal);
-    }
-
-    public void goToMechanismActivity(View view) {
-        Intent toMechanism = new Intent(this, MechanismActivity.class);
-        startActivity(toMechanism);
-    }
-
-    public void GoToLink2(View view) {
-        Intent toLink2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
-        startActivity (toLink2);
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id. MainButton1:
+                // If MainButton1 is clicked, do something
+                /*
+                Intent toLog = new Intent(this, LogActivity.class);
+                startActivity(toLog);
+                */
+                Intent toLog = new Intent(this, LogActivity.class);
+                startActivity(toLog);
+                break;
+            case R.id.MainButton2:
+                // if MainButton2 is clicked, do something
+                Intent toNormal = new Intent(this, NormalActivity.class);
+                startActivity(toNormal);
+                break;
+            case R.id.MainButton3:
+                // if MainButton3 is clicked, do something
+                Intent toMechanism = new Intent(this, MechanismActivity.class);
+                startActivity(toMechanism);
+                break;
+            case R.id.MainButton4:
+                // if MainButton4 is clicked, do something
+                Intent toLink2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
+                startActivity (toLink2);
+                break;
+        }
     }
 }
