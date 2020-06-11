2adfcb5ad62617a7512a3045f5d526825844e07f
ippei94da <ippei94da@gmail.com>
2020-06-11 20:21:16 +0900

    mplemented data transfer using Intent in LogActivity

--- app/src/main/java/com/example/bttracker/LogActivity.java
+++ app/src/main/java/com/example/bttracker/LogActivity.java
@@ -1,14 +1,31 @@
 package com.example.bttracker;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class LogActivity extends AppCompatActivity {
+
+    EditText userInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        // get the ID of the EditText on the layout
+        userInput = (EditText)findViewById(R.id.LogInputData);
+    }
+
+    public void confirmBT(View view) {
+        Intent toConfirm = new Intent(this, ConfirmActivity.class);
+        // append user input to the toConfirm Intent
+        // the 2nd argument means: get user input from the EditText View,
+        // and convert the text to a string
+        toConfirm.putExtra("BT_data", userInput.getText().toString());
+        // startConfirmActivity
+        startActivity(toConfirm);
     }
 }
